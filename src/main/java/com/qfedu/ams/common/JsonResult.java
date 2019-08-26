f2ac6dc36b3333e2f5a5bd18d727bab5fad4f2aa
carlos <1417410557@qq.com>
2019-08-26 21:22:47 +0800

    更新公共类

---
+++ src/main/java/com/qfedu/ams/common/JsonResult.java
@@ -0,0 +1,36 @@
+package com.qfedu.ams.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Carlos
+ * Date: 2019/8/26
+ * Time: 21:20
+ */
+public class JsonResult {
+    private int code;
+    private Object info;
+
+    public JsonResult() {
+    }
+
+    public JsonResult(int code, Object info) {
+        this.code = code;
+        this.info = info;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Object getInfo() {
+        return info;
+    }
+
+    public void setInfo(Object info) {
+        this.info = info;
+    }
+}
