2fc57168e6ed977e48f8a829effa0394619ed9de
carlos <1417410557@qq.com>
2019-08-28 22:02:55 +0800

    更新映射到dao接口的注解

--- src/main/java/com/qfedu/ams/AmsApplication.java
+++ src/main/java/com/qfedu/ams/AmsApplication.java
@@ -1,9 +1,11 @@
 package com.qfedu.ams;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qfedu.ams.dao")
 public class AmsApplication {
 
     public static void main(String[] args) {
