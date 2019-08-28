d12d660ff195243e7b95ccce30d179619121ec80
xx <992034152@qq.com>
2019-08-28 19:45:57 +0800

    1.1

--- src/main/java/com/qfedu/ams/entity/ExamSubject.java
+++
@@ -1,9 +0,0 @@
-package com.qfedu.ams.entity;
-
-import lombok.Data;
-//学科表
-@Data // 自动生成对应的getter和setter方法
-public class ExamSubject {
-    private Integer subjectId;//学科ID
-    private String subjectName;//学科名字
-}
