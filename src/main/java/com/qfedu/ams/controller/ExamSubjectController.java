db5cd2c3c5e12a5cbc887856ecd9ae01cd16bb54
xx <992034152@qq.com>
2019-08-29 20:02:22 +0800

    关闭过滤器

--- src/main/java/com/qfedu/ams/controller/ExamSubjectController.java
+++ src/main/java/com/qfedu/ams/controller/ExamSubjectController.java
@@ -4,8 +4,10 @@ import com.qfedu.ams.common.JsonResult;
 import com.qfedu.ams.entity.ExamSubject;
 import com.qfedu.ams.service.ExamSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.spring.web.json.Json;
 
 import java.util.List;
 
@@ -14,18 +16,28 @@ import java.util.List;
  *
  * @author xx
  */
-@RestController("/ES")
+@Controller
 public class ExamSubjectController {
 
     @Autowired
     private ExamSubjectService examSubjectService;
 
-    @RequestMapping("/list")
+    @RequestMapping("/ES/list.do")
+    @ResponseBody
     public JsonResult listES(){
         List<ExamSubject> list = examSubjectService.findAll();
         return new JsonResult(1, list);
     }
 
+    @RequestMapping("/ES/add.do")
+    public String addES(ExamSubject examSubject){
+        examSubjectService.insert(examSubject);
+        return "redirect:/ES/list.do";
+    }
+
+//    public JsonResult deleteES(Integer id){
+//
+//    }
 
 
 }
