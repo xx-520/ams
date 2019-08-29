1f078470644f59161941d5eb34e5241e7f7bf635
xx <992034152@qq.com>
2019-08-29 15:29:16 +0800

    修改学科dao层和mapper层

--- src/main/java/com/qfedu/ams/dao/ExamSubjectMapper.java
+++ src/main/java/com/qfedu/ams/dao/ExamSubjectMapper.java
@@ -2,16 +2,42 @@ package com.qfedu.ams.dao;
 
 import com.qfedu.ams.entity.ExamSubject;
 
+/**
+ * 学科管理
+ *
+ * @author xx
+ */
 public interface ExamSubjectMapper {
+
+    /**
+     * 删除学科信息
+     *
+     * @param subjectid 传入一个学科 id
+     * @return
+     */
     int deleteByPrimaryKey(Integer subjectid);
 
+    /**
+     * 添加学科信息
+     *
+     * @param record 传入一个完整的 record对象
+     * @return
+     */
     int insert(ExamSubject record);
 
-    int insertSelective(ExamSubject record);
-
+    /**
+     * 查找指定学科信息
+     *
+     * @param subjectid 传入学科ID
+     * @return
+     */
     ExamSubject selectByPrimaryKey(Integer subjectid);
 
-    int updateByPrimaryKeySelective(ExamSubject record);
-
+    /**
+     * 修改学科信息
+     *
+     * @param record 传入 record对象
+     * @return
+     */
     int updateByPrimaryKey(ExamSubject record);
 }
