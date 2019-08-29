920f46bd035c817a999d3532738f63e0fa15d243
xx <992034152@qq.com>
2019-08-29 15:58:49 +0800

    学科信息修改

---
+++ src/main/java/com/qfedu/ams/controller/ExamSubjectController.java
@@ -0,0 +1,4 @@
+package com.qfedu.ams.controller;
+
+public class ExamSubjectController {
+}
--- src/main/java/com/qfedu/ams/service/impl/ExamSubjectServiceImpl.java
+++ src/main/java/com/qfedu/ams/service/impl/ExamSubjectServiceImpl.java
@@ -1,4 +1,39 @@
 package com.qfedu.ams.service.impl;
 
-public class ExamSubjectServiceImpl {
+import com.qfedu.ams.dao.ExamSubjectMapper;
+import com.qfedu.ams.entity.ExamSubject;
+import com.qfedu.ams.service.ExamSubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 学科实现层
+ *
+ * @author xx
+ */
+@Service
+public class ExamSubjectServiceImpl implements ExamSubjectService {
+
+    @Autowired(required = false)
+    private ExamSubjectMapper examSubjectMapper;
+
+    @Override
+    public int deleteByPrimaryKey(Integer subjectid) {
+        return examSubjectMapper.deleteByPrimaryKey(subjectid);
+    }
+
+    @Override
+    public int insert(ExamSubject record) {
+        return examSubjectMapper.insert(record);
+    }
+
+    @Override
+    public ExamSubject selectByPrimaryKey(Integer subjectid) {
+        return examSubjectMapper.selectByPrimaryKey(subjectid);
+    }
+
+    @Override
+    public int updateByPrimaryKey(ExamSubject record) {
+        return examSubjectMapper.updateByPrimaryKey(record);
+    }
 }
