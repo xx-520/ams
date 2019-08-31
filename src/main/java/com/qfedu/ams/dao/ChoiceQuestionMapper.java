103c11c0e93f49d16d3b5ccee96c92535244c2bc
xx <992034152@qq.com>
2019-08-31 09:42:49 +0800

    删除登录各种信息

--- src/main/java/com/qfedu/ams/dao/ChoiceQuestionMapper.java
+++ src/main/java/com/qfedu/ams/dao/ChoiceQuestionMapper.java
@@ -2,11 +2,15 @@ package com.qfedu.ams.dao;
 
 import com.qfedu.ams.entity.ChoiceQuestion;
 
+import java.util.List;
+
 /**
  * 选择题 dao层
- *
  */
 public interface ChoiceQuestionMapper {
+
+    List<ChoiceQuestion> findAll(ChoiceQuestion choiceQuestion);
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(ChoiceQuestion record);
